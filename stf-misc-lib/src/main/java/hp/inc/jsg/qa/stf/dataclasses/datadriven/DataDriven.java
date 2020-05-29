bfcb4de2045c2489c95da2b5a8e7fb4f975ee699
Giacomin <giacomin@hp.com>
2020-05-29 01:10:31 -0300

    Adding proper copyright header

--- stf-misc-lib/src/main/java/hp/inc/jsg/qa/stf/dataclasses/datadriven/DataDriven.java
+++ stf-misc-lib/src/main/java/hp/inc/jsg/qa/stf/dataclasses/datadriven/DataDriven.java
@@ -1,21 +1,10 @@
 package hp.inc.jsg.qa.stf.dataclasses.datadriven;
 
 /************************************************************
- * Smart Test Framework
+ *  © Copyright 2019 HP Development Company, L.P.
+ *  SPDX-License-Identifier: MIT
  *
- * Copyright (c) Hewlett-Packard Company, Aug / 2019
- * All rights are reserved.  Copying or other reproduction of
- * this program except for archival purposes is prohibited
- * without the prior written consent of Hewlett-Packard Company.
- *
- *RESTRICTED RIGHTS LEGEND
- * Use, duplication, or disclosure by the Government
- * is subject to restrictions as set forth in
- * paragraph (b) (3) (B) of the Rights in Technical
- * Data and Computer Software clause in DAR 7-104.9(a).
- *
- * HEWLETT-PACKARD COMPANY
- *
+ *  Smart Test Framework
  ************************************************************/
 public class DataDriven {
     public SmartDataDriven[] SmartDataDriven;
