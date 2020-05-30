111314aeaa900dfb5e49a5389169faca80ca6d8e
Giacomin <giacomin@hp.com>
2020-05-30 12:10:37 -0300

    removing old copyright from tests

--- stf-http-client-lib/src/test/java/system/test/base/MainTestBase.java
+++ stf-http-client-lib/src/test/java/system/test/base/MainTestBase.java
@@ -7,23 +7,6 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 
-/************************************************************
- * Smart Test Framework
- *
- * Copyright (c) Hewlett-Packard Company, Aug / 2019
- * All rights are reserved.  Copying or other reproduction of
- * this program except for archival purposes is prohibited
- * without the prior written consent of Hewlett-Packard Company.
- *
- *RESTRICTED RIGHTS LEGEND
- * Use, duplication, or disclosure by the Government
- * is subject to restrictions as set forth in
- * paragraph (b) (3) (B) of the Rights in Technical
- * Data and Computer Software clause in DAR 7-104.9(a).
- *
- * HEWLETT-PACKARD COMPANY
- *
- ************************************************************/
 public class MainTestBase {
 
     protected static TestLog testLog;
