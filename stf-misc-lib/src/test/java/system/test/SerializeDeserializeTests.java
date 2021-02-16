b700a143d232f51691bc6fe762b8c1f74c32ad81
Giacomin <giacomin@hp.com>
2021-02-16 18:54:32 -0300

    Adding new parsers tests for json path api helper

--- stf-misc-lib/src/test/java/system/test/SerializeDeserializeTests.java
+++ stf-misc-lib/src/test/java/system/test/SerializeDeserializeTests.java
@@ -4,14 +4,20 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.github.jeansantos38.stf.enums.serialization.SerializationType;
 import com.github.jeansantos38.stf.framework.io.InputOutputHelper;
 import com.github.jeansantos38.stf.framework.serialization.DeserializeHelper;
+import com.github.jeansantos38.stf.framework.serialization.JsonParserHelper;
 import com.github.jeansantos38.stf.framework.serialization.SerializeHelper;
+import com.jayway.jsonpath.Option;
 import io.qameta.allure.*;
+import net.minidev.json.JSONArray;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import system.base.MainTestBase;
 import system.pojo.NoteDemoClass;
+import system.pojo.Book;
 
 import java.io.File;
+import java.lang.String;
+import java.util.List;
 
 
 public class SerializeDeserializeTests extends MainTestBase {
@@ -92,6 +98,69 @@ public class SerializeDeserializeTests extends MainTestBase {
         NoteDemoClass deserializedDemoClass = DeserializeHelper.deserializeStringToObject(NoteDemoClass.class, SerializationType.YAML, serializedString);
 
         compareSerializedAndDeserializedObjects(deserializedDemoClass, noteDemoClass);
+    }
+
+    @Test
+    @Severity(SeverityLevel.BLOCKER)
+    @Story("The serialize helper should work for yaml files")
+    @Description("Serialize a yaml object and check all its properties values")
+    @Link(name = "This could be a Link to your project Issue Tracker", url = "https://github.com/HPInc/smart-test-framework")
+    public void parseJsonDeserializeTest() throws Exception {
+        String content = InputOutputHelper.readContentFromFile(discoverAbsoluteFilePath("booksStore.json"));
+        Book book = JsonParserHelper.deserializeJsonPath(Book.class, "$.store.book[3]", content);
+        Assert.assertEquals(book.author, "J. R. R. Tolkien");
+        Assert.assertEquals(book.title, "The Lord of the Rings");
+        Assert.assertEquals(book.isbn, "0-395-19395-8");
+    }
+
+    @Test
+    @Severity(SeverityLevel.BLOCKER)
+    @Story("The serialize helper should work for yaml files")
+    @Description("Serialize a yaml object and check all its properties values")
+    @Link(name = "This could be a Link to your project Issue Tracker", url = "https://github.com/HPInc/smart-test-framework")
+    public void parseJsonReadDotNotationTest() throws Exception {
+        String content = InputOutputHelper.readContentFromFile(discoverAbsoluteFilePath("booksStore.json"));
+        String isbn = (String) JsonParserHelper.readJsonPath("$.store.book[3].isbn", content);
+        String author = (String) JsonParserHelper.readJsonPath("$.store.book[3].author", content);
+        String title = (String) JsonParserHelper.readJsonPath("$.store.book[3].title", content);
+
+        Assert.assertEquals(author, "J. R. R. Tolkien");
+        Assert.assertEquals(title, "The Lord of the Rings");
+        Assert.assertEquals(isbn, "0-395-19395-8");
+    }
+
+    @Test
+    @Severity(SeverityLevel.BLOCKER)
+    @Story("The serialize helper should work for yaml files")
+    @Description("Serialize a yaml object and check all its properties values")
+    @Link(name = "This could be a Link to your project Issue Tracker", url = "https://github.com/HPInc/smart-test-framework")
+    public void parseJsonReadBracketNotationTest() throws Exception {
+        String content = InputOutputHelper.readContentFromFile(discoverAbsoluteFilePath("booksStore.json"));
+
+        String author = (String) JsonParserHelper.readJsonPath("$['store']['book'][3]['author']", content);
+        Assert.assertEquals(author, "J. R. R. Tolkien");
+
+        String notExist = (String) JsonParserHelper.readJsonPath("$['store']['book'][3]['notExist']", content, Option.DEFAULT_PATH_LEAF_TO_NULL);
+        Assert.assertNull(notExist);
+
+        List<String> author2 = (List<String>) JsonParserHelper.readJsonPath("$['store']['book'][3]['author']", content, Option.ALWAYS_RETURN_LIST);
+        Assert.assertEquals(author2.get(0), "J. R. R. Tolkien");
+
+        // TODO complete this test - WIP
+        List<String> author3 = (List<String>) JsonParserHelper.readJsonPath("$['store']['book'][3]", content, Option.ALWAYS_RETURN_LIST);
+        JSONArray author4 = JsonParserHelper.tryReadJsonPath("$['store']['book'][3]['author']", content);
+        JSONArray book = JsonParserHelper.tryReadJsonPath("$['store']['book'][3]", content);
+        String books = JsonParserHelper.tryParseJsonPathToString("$['store']['book']", content);
+        String booksx = JsonParserHelper.tryParseJsonPathToString("$['store']['book'][3]", content);
+        String booksx2 = JsonParserHelper.tryParseJsonPathToString("$['store']['book'][3]['author']", content);
+
+        Assert.assertEquals(author2.get(0), "J. R. R. Tolkien");
+
+        String title = (String) JsonParserHelper.readJsonPath("$['store']['book'][3]['title']", content);
+        String isbn = (String) JsonParserHelper.readJsonPath("$['store']['book'][3]['isbn']", content);
+
+        Assert.assertEquals(title, "The Lord of the Rings");
+        Assert.assertEquals(isbn, "0-395-19395-8");
     }
 
     private void assertAllPropertiesValuesFromNoteObject(NoteDemoClass noteDemoClass) {
